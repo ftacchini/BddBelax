33ed82020c508569d6000652aee73892e66038d8
Federico Tacchini <tacchinifederico@gmail.com>
2016-06-07 23:54:57 -0300

    consolerunner3

--- MathOperations/RunSpecflow.bat
+++ MathOperations/RunSpecflow.bat
@@ -1,4 +1,4 @@
-"%~dp0\packages\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe" /out=TestResult.txt /result=TestResult.xml /framework=net-4.5 MathOperations.AcceptanceTests\bin\Release\AccptanceTests.dll
+"%~dp0\packages\NUnit.Console.3.0.1\tools\nunit3-console.exe" /out=TestResult.txt /result=TestResult.xml /framework=net-4.5 MathOperations.AcceptanceTests\bin\Release\AccptanceTests.dll
  
 "%~dp0\packages\SpecFlow.2.1.0\tools\specflow.exe" nunitexecutionreport MathOperations.AcceptanceTests\MathOperations.AcceptanceTests.csproj
  
